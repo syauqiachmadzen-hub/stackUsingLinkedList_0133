908009f36f9ae73be06008b4e92f6cef360399b8
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-14 10:40:07 +0700

    stackUsingLinkedList 17

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -57,7 +57,11 @@ public:
         {
             Node *current = top;
             while (current != NULL)
-            
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
         }
     }
 }
